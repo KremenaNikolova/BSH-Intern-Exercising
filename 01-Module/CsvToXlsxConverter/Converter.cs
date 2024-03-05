811cb3e97b3857e80c0957b843d7e189d7bf3d61
Kremena Nikolova <aradia1992@gmail.com>
2024-03-05 21:45:50 +0200

    install EPPlus package and implement converting

---
+++ 01-Module/CsvToXlsxConverter/Converter.cs
@@ -0,0 +1,31 @@
+﻿namespace CsvToXlsxConverter
+{
+    using OfficeOpenXml;
+
+    public class Converter
+    {
+        public void ConvertCsvToExcel(string csvFilePath, string excelFilePath)
+        {
+            FileInfo excelFile = new FileInfo(excelFilePath);
+
+            using ExcelPackage package = new ExcelPackage(excelFile);
+
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+            string[] csvLines = File.ReadAllLines(csvFilePath);
+
+            int row = 1;
+            foreach (string csvLine in csvLines)
+            {
+                string[] fields = csvLine.Split('|');
+                for (int i = 0; i < fields.Length; i++)
+                {
+                    worksheet.Cells[row, i + 1].Value = fields[i];
+                }
+                row++;
+            }
+
+            package.Save();
+        }
+    }
+}
--- 01-Module/CsvToXlsxConverter/Program.cs
+++ 01-Module/CsvToXlsxConverter/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace CsvToXlsxConverter
 {
+    using OfficeOpenXml;
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            string file = Input.GetFile();
+            string output = Output.GetOutput();
+
+            Converter converter = new Converter();
+            converter.ConvertCsvToExcel(file, output);
+
+            Console.WriteLine("CSV file has been converted to Excel successfully.");
         }
+
+        
     }
 }
