77044a99cf156b2f5351dea19e2f36dadab21e8d
Kremena Nikolova <aradia1992@gmail.com>
2024-03-05 21:45:59 +0200

    add exception message

---
+++ 01-Module/CsvToXlsxConverter/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace CsvToXlsxConverter
+{
+    public static class ExceptionMessages
+    {
+        public static string WrongInputMessage = "Something get wrong. Please relunch the program and try again.";
+    }
+}
