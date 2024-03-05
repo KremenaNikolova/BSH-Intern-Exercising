9b84e9e9224f2ee4604b2beb8ea8a3d9ae2f9368
Kremena Nikolova <aradia1992@gmail.com>
2024-03-05 21:45:08 +0200

    add input and output class

---
+++ 01-Module/CsvToXlsxConverter/Input.cs
@@ -0,0 +1,20 @@
+﻿namespace CsvToXlsxConverter
+{
+    public class Input
+    {
+        public static string GetFile()
+        {
+            Console.WriteLine("Please enter the .csv File Path!");
+            string file = Console.ReadLine()!;
+
+            while (!File.Exists(file))
+            {
+                Console.WriteLine("Wrong File Path \n" + "Please enter valid File Path!");
+                file = Console.ReadLine()!;
+            }
+
+
+            return file;
+        }
+    }
+}
---
+++ 01-Module/CsvToXlsxConverter/Output.cs
@@ -0,0 +1,22 @@
+﻿namespace CsvToXlsxConverter
+{
+    public class Output
+    {
+        public static string GetOutput()
+        {
+            Console.WriteLine("Please enther name of the .xlsx file");
+            string newFileName = Console.ReadLine()!;
+            string excelFilePath = $"..\\..\\..\\{newFileName}.xlsx";
+
+            while (File.Exists(excelFilePath))
+            {
+                Console.WriteLine("File with that name already exist. \n" + "Please try with another name!");
+                newFileName = Console.ReadLine()!;
+                excelFilePath = $"..\\..\\..\\{newFileName}.xlsx";
+            }
+
+
+            return excelFilePath;
+        }
+    }
+}
