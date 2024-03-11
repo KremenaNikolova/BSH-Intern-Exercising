939b2d69ac3e3a1803c8211f104fec517b294637
Kremena Nikolova <aradia1992@gmail.com>
2024-03-11 13:42:15 +0200

    insert more dummy data

--- 03-Module/03-Insert-Data.sql
+++ 03-Module/03-Insert-Data.sql
@@ -18,3 +18,35 @@ SELECT [First Name]
       ,[Phone]
       ,PARSE([Birthday] AS DATE USING 'AR-LB')
   FROM [Data]
+
+
+ INSERT INTO [Categories](
+	    [Category Name]
+		)
+ VALUES ('Cloths'),
+	    ('Toys'),
+		('Food'),
+		('Drinks'),
+		('Others')
+
+
+ INSERT INTO [Products](
+		[Product Name]
+	   ,[Category]
+	   ,[CategoryId]
+	   ,[Price]
+	   ,[Quantity]
+	   ,[CustomerId]
+	    )
+ VALUES ('Shirt', 'Cloths', 1, 5.55, 3, 3),
+		('Jeans', 'Cloths', 1, 15.32, 25, 1),
+		('Dog-Toy', 'Toys', 2, 3.00, 2, 25),
+		('Ball', 'Toys', 2, 1.55, 50, 15),
+		('Hamburger', 'Food', 3, 4.25, 3, 33),
+		('Hamburger', 'Food', 3, 4.25, 1, 30),
+		('Coca-Cola', 'Drinks', 4, 2.60, 2, 9),
+		('Coca-Cola', 'Drinks', 4, 2.60, 3, 12),
+		('Coca-Cola', 'Drinks', 4, 2.60, 12, 22),
+		('Random-thing', 'Others', 5, 123.33, 1, 3),
+		('Random-thing', 'Others', 5, 123.33, 1, 25)
+
