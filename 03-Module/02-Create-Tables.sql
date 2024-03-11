a61e64c5b8e13fe75ada7beb841f95dc242245bd
Kremena Nikolova <aradia1992@gmail.com>
2024-03-11 09:21:24 +0200

    add tables

---
+++ 03-Module/02-Create-Tables.sql
@@ -0,0 +1,30 @@
+USE [Online Shop]
+
+
+CREATE TABLE [Categories](
+	   [Id] INT PRIMARY KEY IDENTITY,
+	   [Category Name] NVARCHAR(50) NOT NULL
+	   )
+
+CREATE TABLE [Customers](
+	   [Id] INT PRIMARY KEY IDENTITY,
+	   [First Name] NVARCHAR(50) NOT NULL,
+	   [Last Name] NVARCHAR(50) NOT NULL,
+	   [Email] VARCHAR(80) NOT NULL,
+	   [Gender] VARCHAR(10) NOT NULL,
+	   [Country] NVARCHAR(100) NOT NULL,
+	   [City] NVARCHAR(100) NOT NULL,
+	   [Phone] VARCHAR(15) NOT NULL,
+	   [Birthdate] DATETIME NOT NULL
+	   )
+
+	
+CREATE TABLE [Products](
+	   [Id] INT PRIMARY KEY IDENTITY,
+	   [Product Name] NVARCHAR(100) NOT NULL,
+	   [Category] NVARCHAR(50) NOT NULL,
+	   [CategoryId] INT FOREIGN KEY REFERENCES [Categories],
+	   [Price] DECIMAL NOT NULL,
+	   [Quantity] INT NOT NULL,
+	   [CustomerId] INT FOREIGN KEY REFERENCES [Customers]
+	   )
