b53398aa7c721d81c29cd6beece7a0fee1d0a3b0
Kremena Nikolova <aradia1992@gmail.com>
2024-03-12 20:23:02 +0200

    update the queries using new CustomersProducts table

--- 03-Module/05-Select-Customers-Products.sql
+++ 03-Module/05-Select-Customers-Products.sql
@@ -2,5 +2,7 @@ SELECT CONCAT(c.[First Name], ' ', c.[Last Name]) as [Customer Name],
 	   p.[Product Name],
        p.[Category]
   FROM [Customers] AS c
-LEFT JOIN [Products] AS p
-    ON c.Id = p.CustomerId
+  LEFT JOIN [CustomersProducts] AS cp
+    ON c.[Id] = cp.[CustomerId]
+  LEFT JOIN [Products] AS p
+	ON cp.[ProductId] = p.[Id]
--- 03-Module/06-Most-Wanted-Product.sql
+++ 03-Module/06-Most-Wanted-Product.sql
@@ -1,3 +1,7 @@
-SELECT TOP(1) *
-  FROM [Products] as p
- ORDER BY p.Quantity DESC
+SELECT TOP(1)
+	   p.[Product Name]
+	  ,SUM(cp.[Quantity]) as [Total Sells Quantity]
+  FROM [CustomersProducts] AS cp
+  JOIN [Products] as p
+    ON p.[Id] = cp.[ProductId]
+ GROUP BY p.[Product Name]
