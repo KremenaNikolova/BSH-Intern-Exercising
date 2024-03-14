5bbefaf6f3cc0a34c3e540d0d3c0606337bb6e23
Kremena Nikolova <aradia1992@gmail.com>
2024-03-14 15:28:58 +0200

    project init

---
+++ 04-Module/CustomerApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
